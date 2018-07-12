7bb5044651f0d9d11705c2efc5db9244da916aa0
ewinds <dutboxer@gmail.com>
2018-07-12 15:01:01 +0800

    新增测试用消费者

--- p2bp-account/src/main/java/io/github/ewinds/config/MessagingConfiguration.java
+++ p2bp-account/src/main/java/io/github/ewinds/config/MessagingConfiguration.java
@@ -1,16 +1,17 @@
 package io.github.ewinds.config;
 
+import io.github.ewinds.messaging.ConsumerChannel;
 import io.github.ewinds.messaging.ProducerChannel;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 
 /**
  * Configures Spring Cloud Stream support.
- *
+ * <p>
  * See http://docs.spring.io/spring-cloud-stream/docs/current/reference/htmlsingle/
  * for more information.
  */
-@EnableBinding(value = {Source.class, ProducerChannel.class})
+@EnableBinding(value = {Source.class, ProducerChannel.class, ConsumerChannel.class})
 public class MessagingConfiguration {
 
 }
---
+++ p2bp-account/src/main/java/io/github/ewinds/messaging/ConsumerChannel.java
@@ -0,0 +1,11 @@
+package io.github.ewinds.messaging;
+
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.messaging.SubscribableChannel;
+
+public interface ConsumerChannel {
+    String CHANNEL = "subscribableChannel";
+
+    @Input
+    SubscribableChannel subscribableChannel();
+}
